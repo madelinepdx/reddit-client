748b1b83c7fab542c3e3f3ede71476c270f64c8b
Maddy <madelinepdx@gmail.com>
2024-07-09 18:16:16 +0200

    Update to React 18 createRoot API and ensure Redux Provider setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import store from './store';
+import { Provider } from 'react-redux';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
+
+// Register the service worker
+serviceWorkerRegistration.register();
